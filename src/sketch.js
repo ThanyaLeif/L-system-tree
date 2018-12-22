43ed0a5ad9f17efc09d4dc7e59e42c047bf7d82f
ThanyaLeif <32908302+ThanyaLeif@users.noreply.github.com>
2018-12-22 14:54:44 -0600

    Adding initial files

--- src/sketch.js
+++ src/sketch.js
@@ -1,69 +1,53 @@
-var ship;
-var flowers = [];
-var drops = [];
+//Creating fractal trees with L-SysteM
+
+//----------RULES OF L-SYSTEM---------
+//variables: A B
+// axiom: A
+// rules: (A -> AB), (B -> A)
+
+var axiom = "A";
+var sentence = axiom;
+
+//Rule for the convertion of A's
+var rules = [];
+rules[0] = {
+	a: "A",
+	b: "AB"
+}
+
+//Rule for the convertion of B's
+rules[1] = {
+	a: "B",
+	b: "A"
+}
+
+function generate(){
+	var nextSentence = "";
+	for(var i=0; i<sentence.length; i++){
+		var current = sentence.charAt(i);
+		var found = false;
+		for(var j=0; j<rules.length; j++){
+			if(current == rules[j].a){
+				found = true;
+				nextSentence += rules[j].b;
+				break;
+			}
+		}
+		if(!found){
+			nextSentence += current;
+		}
+	}
+	sentence = nextSentence;
+	createP(sentence);
+}
 
 function setup(){
-	createCanvas(600, 400);
-	ship = new Ship();
-	for(var i=0; i<6; i++){
-		flowers[i] = new Flower(i*80 + 80, 30);
-	}
+	noCanvas();
+	createP(axiom); //Print the letter
+	var button = createButton("generate"); //This is in the P5 library
+	button.mousePressed(generate);
 }
 
 function draw(){
-	background(51);
-	ship.show();
-	ship.move(0);
-
-	for(var i=0; i<drops.length; i++){
-		drops[i].show();
-		drops[i].move();
-		for(var j=0; j<flowers.length; j++){
-			if(drops[i].hits(flowers[j])){
-				flowers[j].grow();
-				drops[i].evaporate();
-			}
-		}	
-	}
-
-	var edge = false;
-	for(var i=0; i<flowers.length; i++){
-		flowers[i].show();
-		flowers[i].move();
-
-		if(flowers[i].x + flowers[i].r > width || flowers[i].x - flowers[i].r < 0){
-			edge = true;
-		}
-
-	}
-	if(edge){
-		for(var i=0; i<flowers.length; i++){
-			flowers[i].shiftDown();
-		}
-	}
-
-	for(var i=0; i<drops.length; i++){
-		if(drops[i].toDelete){
-			drops.splice(i, 1);
-		}
-	}
-}
-
-function keyReleased(){
-	if(key != ' '){
-		ship.setDir(0);
-	}
-}
-
-function keyPressed(){
-	if(key == ' '){
-		var drop = new Drop(ship.x + 10, height - 20);
-		drops.push(drop);
-	}
-	if(keyCode == RIGHT_ARROW){
-		ship.setDir(1);
-	}
-	if(keyCode == LEFT_ARROW){
-		ship.setDir(-1);
-	}
+	
 }
